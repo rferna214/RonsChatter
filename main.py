16ea9802e7c99b3cdab2140d9667275178fecacb
rferna214 <rferna214@gmail.com>
2026-04-02 20:40:26 -0400

    day 3 done

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import requests
 #Exit stops the page from executing. kinda like a break
 
 
+
+def get_user():
+    response = requests.get("https://randomuser.me/api/")
+    data = response.json()
+    return data["results"][0]
+
+def format_user(user):
+    first_name = user["name"]["first"]
+    last_name = user["name"]["last"]
+    country = user["location"]["country"]
+    age = user["dob"]["age"] 
+
+    if age < 18:
+        status = "Minor"
+    else:
+        status = "Adult"
+
+    return first_name, last_name, country, age, status
+
+
 try:
     count = int(input("Put in a number to loop?"))
 except:
@@ -13,26 +33,29 @@ if count > 10:
     print("Nope!!!!")
     exit()
 
-
-print(count)
+ 
 
 for i in range(count):
-    response = requests.get("https://randomuser.me/api/")
-    data = response.json()
+
+    user = get_user()
+    first_name, last_name, country, age, status = format_user(user)
+
+    #response = requests.get("https://randomuser.me/api/")
+    #data = response.json()
 
     #print(data)
 
-    user = data["results"][0]
+    #user = data["results"][0]
 
-    first_name = user["name"]["first"]
-    last_name = user["name"]["last"]
-    country = user["location"]["country"]
-    age = user["dob"]["age"] 
+    #first_name = user["name"]["first"]
+    #last_name = user["name"]["last"]
+    #country = user["location"]["country"]
+    #age = user["dob"]["age"] 
 
-    if age < 18:
-        status ="Minor"
-    else:
-        status = "Adult"
+    #if age < 18:
+    #    status ="Minor"
+    #else:
+    #    status = "Adult"
 
 
     print(f"Name: {first_name} {last_name}")
@@ -40,7 +63,4 @@ for i in range(count):
     print(f"Age: {age} ") 
     print(f"Status: {status} ") 
     print("\n-----\n")
-
-
-
 
